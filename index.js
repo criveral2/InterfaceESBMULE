a9d1a0c93a319558c7d516d11a347ee2c31d136a
Christian Rivera <you@example.com>
2021-05-19 13:01:13 -0500

    cambios

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+
 const express = require('express');
 const app = express();
-
+let json;
 
 app.set('views', './public/views');
 
@@ -14,5 +15,42 @@ app.get('/', (req, res) => {
 });
 
 
+const request = require('request');
+
+const options = {
+    url: 'http://localhost:8081/Administracion?servicio=productos',
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+        'Accept-Charset': 'utf-8',
+        'User-Agent': 'my-reddit-client'
+    }
+};
+
+
+
+
+if (typeof localStorage === "undefined" || localStorage === null) {
+    var LocalStorage = require('node-localstorage').LocalStorage;
+    localStorage = new LocalStorage('./scratch');
+
+    request(options, function(err, res, body) {
+
+        let json = JSON.parse(body);
+        localStorage.setItem('myFirstKey', JSON.stringify(json));
+        //console.log(localStorage.getItem('myFirstKey'));
+        console.log(json);
+    });
+}
+
+
+   
+
+
+
+
+
+
 const port = 3030;
 app.listen(port, () => console.log(`App listening on port ${port}`))
+
